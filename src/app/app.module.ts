05fbb51d0c3de0ece6dcde83cdad6167b5d41f94
Sebastian Ortega <sebastian.ortega.fernandezz@gmail.com>
2023-10-30 17:11:35 -0300

    fin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +12,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModule,
+    DbzModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
